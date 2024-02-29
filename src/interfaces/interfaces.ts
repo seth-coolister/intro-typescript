d783622a4447c4a63b8eafde7ab308f474dfe5be
Seth Callister <seth.callister3@gmail.com>
2024-02-29 14:03:46 -0700

    reading json data, building nav bar

--- src/interfaces/interfaces.ts
+++ src/interfaces/interfaces.ts
@@ -1,3 +1,56 @@
+import curriculum from '../data/curriculum.json';
+
+const blocks = curriculum['responsive-web-design'].blocks;
+
+const blocksArray = Object.values(blocks);
+
+const [basicCss, basicHTML, flexbox, grid] = blocksArray; // destructuring
+
+// console.log(basicCss);
+
+const navBar = document.querySelector('.navbar');
+
+const buildTopNavFromJSON = (blocksArray: any) => {
+    blocksArray.forEach((block: any, index: Number) => {
+        console.log(block.meta.name)
+        const navItem = document.createElement('a');
+        navItem.classList.add('btn','btn-sm','w-32','m-2');
+        navItem.href = `#${block.meta.name}`;
+        navItem.textContent = block.meta.name;
+        navBar?.appendChild(navItem);
+    })
+}
+
+buildTopNavFromJSON(blocksArray);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 /* 
 A Duck object needs to be able to quack and fly and swim. We can define an interface for the Duck object that specifies the methods that the Duck object should have. We can then use the interface to define the Duck object. This is a simple example of how interfaces can be used to define objects in TypeScript.
