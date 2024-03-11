8753f02ab24039d128250fe55992025d6a3a15bc
Seth Callister <seth.callister3@gmail.com>
2024-03-11 16:13:02 -0600

    Update interface layout and functionality

--- src/interfaces/interfaces.ts
+++ src/interfaces/interfaces.ts
@@ -1,40 +1,102 @@
 import curriculum from '../data/curriculum.json';
+import { CodeSnippet } from '../lib/classes/CodeSnippet';
+import hljs from "highlight.js/lib/core";
+import typescript from "highlight.js/lib/languages/typescript";
+hljs.registerLanguage("typescript", typescript);
 
 const blocks = curriculum['responsive-web-design'].blocks;
 
 const blocksArray = Object.values(blocks);
 
-const [basicCss, basicHTML, flexbox, grid] = blocksArray; // destructuring
+const [basicCss, basicHTML, flexbox, grid] = blocksArray; // example of destructuring an array
 
-// console.log(basicCss);
+let { meta, challenges } = basicCss; // example of destructuring an object
 
 const navBar = document.querySelector('.navbar');
+const navList = document.querySelector('#navList');
+const snippetDisplay = document.querySelector('#snippetDisplay');
 
 const buildTopNavFromJSON = (blocksArray: any) => {
     blocksArray.forEach((block: any, index: Number) => {
-        console.log(block.meta.name)
+        //console.log(block.meta.name)
         const navItem = document.createElement('a');
         navItem.classList.add('btn','btn-sm','w-32','m-2');
-        navItem.href = `#${block.meta.name}`;
+        navItem.href = `#${block.meta.dashedName}`;
         navItem.textContent = block.meta.name;
+
+        navItem.addEventListener('click', () => {
+                // find block that matches clicked nav item
+                blocksArray.forEach((item:any) => {
+                const navItem = document.querySelector(`a[href="#${item.meta.dashedName}"]`);
+                navItem?.classList.remove('bg-primary','text-blue-900');
+            })
+            navItem.classList.add('bg-primary','text-blue-900');
+
+            buildLeftNavFromJSON(block);
+        })
         navBar?.appendChild(navItem);
     })
 }
 
+const buildLeftNavFromJSON = (block: any) => {
+    while (navList?.firstChild) {
+        navList.removeChild(navList.firstChild);
+    }
+    block.challenges.forEach((challenge: any) => {
+        const listItem = document.createElement('li');
+        listItem.textContent = challenge.title;
+        listItem.classList.add('btn', 'btn-sm','w-60','my-1','text-ellipsis','overflow-hidden');
+
+        listItem.addEventListener('click', () => {
+            //console.log(challenge.title);
+            buildSnippetDisplay(challenge);
+            window.scrollTo({top: 0, behavior: 'smooth'})
+        })
+
+        navList?.appendChild(listItem);
+    })
+}
+
+const buildSnippetDisplay = (challenge: any) => {
+    // clear snippet display
+    while (snippetDisplay?.firstChild) {
+        snippetDisplay.removeChild(snippetDisplay.firstChild);
+    }
+    // need instances of a CodeSnippet class to be created here
+    // we will call constructor method of codesnippet class here
+    const snippet = new CodeSnippet(
+        challenge.title, 
+        challenge.description, 
+        challenge.instructions,
+        challenge.solutions[0].indexhtml.contents
+        );
+
+    // make dom elements to display code snippet
+
+    const title = document.createElement('h2');
+    title.textContent = snippet.title;
+    title.classList.add('text-2xl','font-bold','text-center','my-4')
+
+    const description = document.createElement('div');
+    description.innerHTML = snippet.description;
+    // description.innerHTML = hljs.highlight(snippet.description, {language:"typescript"}).value;
+
+    const instructions = document.createElement('div');
+    instructions.innerHTML = snippet.instructions;
+    instructions.classList.add('bg-primary-content','p-4','rounded-2xl','text-secondary','my-4');
+
+    const snippetSolution = document.createElement('div');
+    snippetSolution.innerHTML = snippet.solution;
+
+    snippetDisplay?.appendChild(title);
+    snippetDisplay?.appendChild(description);
+    snippetDisplay?.appendChild(instructions);
+    snippetDisplay?.appendChild(snippetSolution);
+}
+
 buildTopNavFromJSON(blocksArray);
 
-
-
-
-
-
-
-
-
-
-
-
-
+buildLeftNavFromJSON(basicCss);
 
 
 
@@ -86,6 +148,6 @@ class Duck implements CanQuack, CanFly, CanSwim {
 }
 
 const duck = new Duck();
-duck.quack();
-duck.fly();
-duck.swim();
+//duck.quack();
+//duck.fly();
+//duck.swim();
